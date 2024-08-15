82feb03f7cf056c0950b466ccec8ea23a644869d
Игорь Честа <igorche2760@gmail.com>
2024-08-15 20:56:41 +0300

    code update

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,24 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',
-         include(('blog.urls', 'blog'),
-                 namespace='blog_main')
-         ),
-    path('posts/',
-         include(('blog.urls', 'blog'),
-                 namespace='blog_posts')
-         ),
-    path('category/',
-         include(('blog.urls', 'blog'),
-                 namespace='blog_category')
-         ),
+    path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
 ]
-
-'''Я пытался все объединить в include,
-но, исходя из ошибок,
-требуется что-то исправить blog и pages.
-Как результат - я все поломал и запутался, aахахааха.
-Мог ты мне поподробнее объяснить, пожалуйста?
-Очень благодарен! Спасибо тебе!'''
